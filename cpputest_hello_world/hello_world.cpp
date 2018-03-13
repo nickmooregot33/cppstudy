0f9cbaa50573fc449751b379412d9bd0d3095faf
Nick <nickjmoore81@gmail.com>
2018-03-12 19:43:28 -0500

    made it better

--- cpputest_hello_world/hello_world.cpp
+++ cpputest_hello_world/hello_world.cpp
@@ -1,9 +1,9 @@
 #include "hello_world.h"
 
 std::string moops::hello_world::speak() { return std::string{"Hello world"};}
-std::string moops::hello_world::speak(std::string name) { return std::string{"Hello "} + name;}
+std::string moops::hello_world::speak(std::string name) { return std::string{"Hello "} + name + "!\n";}
 void moops::hello_world::write(){this->twriter->write(speak());}
 int moops::hello_world::main(int argc, char** argv){
-	std::cout << "I'm a dufus.";
+	std::cout << "I'm a dufus.\n";
 }
 
--- cpputest_hello_world/main.cpp
+++ cpputest_hello_world/main.cpp
@@ -2,5 +2,6 @@
 
 int main (int argument_count, char** arguments){
 	moops::hello_world hello;
+	std::cout << hello.speak(std::string{"Nick"});
 	return hello.main(argument_count, arguments);
 }
